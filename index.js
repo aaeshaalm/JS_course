1083712209a4cf3582feacd05544362e714a69e5
Aaesha <test@gmail.com>
2023-05-16 12:20:15 +0400

    .

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hello")
+
+var var1 =10;
+var var2 = "hiSt\'ring";
+var var3 = null;
+console.log("the 1st varivale is: "+ var1 + " with the type of: "+ typeof var1)
+console.log("the 2nd varivale is: "+ var2 + " with the type of: "+ typeof var2)
+console.log("the 3rd varivale is: "+ var3 + " with the type of: "+ typeof var3)
+var var4 = [1,3,5];
+console.log("the 4rd varivale is: "+ var4 + " with the type of: "+ typeof var4)
+console.log(var2.indexOf("r"))
+
+console.log("exercise:");
+var myname = "aaesha";
+var age = "22";
+console.log("name: "+ myname + " age: "+age );
+console.log(myname.indexOf("a"));
+console.log("The first letter of my name is: "+ myname.charAt(0))
